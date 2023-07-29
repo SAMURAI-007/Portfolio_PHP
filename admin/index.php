91d96dd463664d1e8f15f3d05abecc47fb04f2fe
SAMURAI JACK <mr.amir1385mm@gmail.com>
2023-07-29 20:27:11 +0330

    Starting the changes

--- admin/index.php
+++ admin/index.php
@@ -1,7 +1,7 @@
 <?php
 include "../admin/dbconfig/config.php";
 
-if($_SESSION['login'] == false){
+if ($_SESSION['login'] == false) {
     header("location:../index.php");
 }
 
@@ -9,22 +9,53 @@ $admin = $conn->prepare("SELECT * FROM admin");
 $admin->execute();
 $admin_display = $admin->fetchAll(PDO::FETCH_ASSOC);
 
-if(isset($_POST['logout'])){
+if (isset($_POST['logout'])) {
     header("location:logout.php");
 }
 
 ?>
 
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="../css/styles.css" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <title>admin dashboard</title>
 </head>
+
 <body>
-    <form method="post">
-        <input type="submit" value="logout" name="logout" >
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Admin dashboard</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="logout.php">logout</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <br>
+    <h3>portfolio management</h3>
+    <form method="post" action="posted.php" class="form-control">
+        <input type="text" placeholder="">
+
     </form>
-    
+
 </body>
+
 </html>
--- admin/login.php
+++ admin/login.php
@@ -15,6 +15,8 @@ if (isset($_POST['sub'])) {
         if ($admins['username'] == $username and $admins['password'] == $password) {
             $_SESSION['login'] = true;
             header("location:index.php");
+        } else {
+            echo '<script>alert("Wrong username or password")</script>';
         }
     }
 }
@@ -36,48 +38,44 @@ if (isset($_POST['sub'])) {
     <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
     <!-- Core theme CSS (includes Bootstrap)-->
     <link href="../css/styles.css" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <title>admin login</title>
 </head>
 
 <body>
-    <section class="vh-100">
-        <div class="container-fluid">
-            <div class="container">
-                <div class="col-sm-6 text-black">
+    <div class="container-fluid">
+        <div class="container">
+            <div class="col-sm-6 text-black card" style="margin:inherit; margin-top:100px;">
+                <div class="d-flex align-items-center h-custom-2 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5">
+                    <form method="post" style="width: 23rem;">
 
-                    <div class="px-5 ms-xl-4">
-                        <i class="fas fa-crow fa-2x me-3 pt-5 mt-xl-4" style="color: #709085;"></i>
-                        <span class="h1 fw-bold mb-0">Logo</span>
-                    </div>
+                        <h3 class="fw-normal mb-1" style="letter-spacing: 1px;">Log in
+                            <svg xmlns="http://www.w3.org/2000/svg" width="55" height="55" style="padding: 5px;" fill="currentColor" class="bi bi-box-arrow-in-left" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M10 3.5a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-2a.5.5 0 0 1 1 0v2A1.5 1.5 0 0 1 9.5 14h-8A1.5 1.5 0 0 1 0 12.5v-9A1.5 1.5 0 0 1 1.5 2h8A1.5 1.5 0 0 1 11 3.5v2a.5.5 0 0 1-1 0v-2z" />
+                                <path fill-rule="evenodd" d="M4.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H14.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z" />
+                            </svg>
+                        </h3>
 
-                    <div class="d-flex align-items-center h-custom-2 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5">
-                        <form method="post" style="width: 23rem;">
 
-                            <h3 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Log in</h3>
 
-                            <div class="form-outline mb-4">
-                                <input name="uname" type="text" id="form2Example18" class="form-control form-control-lg" />
-                                <label class="form-label" for="form2Example18">Email address</label>
-                            </div>
+                        <div class="form-outline mb-4">
+                            <input name="uname" type="text" id="form2Example18" class="form-control form-control-lg" />
+                            <label class="form-label" for="form2Example18">Email address</label>
+                        </div>
 
-                            <div class="form-outline mb-4">
-                                <input name="pass" type="password" id="form2Example28" class="form-control form-control-lg" />
-                                <label class="form-label" for="form2Example28">Password</label>
-                            </div>
+                        <div class="form-outline mb-4">
+                            <input name="pass" type="password" id="form2Example28" class="form-control form-control-lg" />
+                            <label class="form-label" for="form2Example28">Password</label>
+                        </div>
 
-                            <div class="pt-1 mb-4">
-                                <button class="btn btn-info btn-lg btn-block" name="sub" type="submit">Login</button>
-                            </div>
-
-                            <p class="small mb-5 pb-lg-2"><a class="text-muted" href="#!">Forgot password?</a></p>
-                            <p>Don't have an account? <a href="#!" class="link-info">Register here</a></p>
-
-                        </form>
-                    </div>
+                        <div class="pt-1 mb-4">
+                            <button class="btn btn-info btn-lg btn-block" name="sub" type="submit">Login</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
 
 </body>
 <!-- Bootstrap core JS-->
@@ -94,5 +92,6 @@ if (isset($_POST['sub'])) {
         window.history.replaceState(null, null, window.location.href);
     }
 </script>
+<script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
 
 </html>
